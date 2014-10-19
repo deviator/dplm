a70194cc22531d6f8e859ccdea32375b0fd9d0d5
deviator <code.viator@gmail.com>
2014-10-19 21:29:23 +0400

    cl works wrong

--- src/draw/worldmap.d
+++ src/draw/worldmap.d
@@ -12,20 +12,46 @@ import des.cl.helpers;
 class CLWorldMap : BaseDrawObject, WorldMap
 {
 protected:
-    GLBuffer data, pnts;
+    class GLCLBuffer : GLBuffer
+    {
+        CLGLMemory clmem;
+
+        this(T)( T[] data )
+        {
+            super( Target.ARRAY_BUFFER );
+            setData( data );
+            createCLMem();
+        }
+
+        void createCLMem()
+        {
+            clmem = registerCLRef( CLGLMemory.createFromGLBuffer( ctx,
+                        CLMemory.Flags.READ_WRITE, this ) );
+        }
+
+        void acquireFromGL()
+        { clmem.acquireFromGL( cmdqueue ); }
+
+        void releaseToGL()
+        { clmem.releaseToGL( cmdqueue ); }
+    }
+
+    GLCLBuffer data, pnts;
 
     mat4 mapmtr;
     mapsize_t mres;
 
     CLGLContext ctx;
-    CLGLMemory clmem;
-    CLKernel update;
     CLCommandQueue cmdqueue;
+
+    CLKernel update;
 
 public:
 
     this( ivec3 res, vec3 cell )
     {
+        /+ TODO: true mapmtr +/
+        matrix = mat4.diag( cell, 1 ).setCol(3, vec4(-res,1) );
         mres = mapsize_t(res*2);
         prepareCL();
 
@@ -34,15 +60,27 @@ public:
         warn_if_empty = false;
     }
 
-    void setPoints( in vec3 from, in vec3[] pnts )
+    void setPoints( in vec3 from, in vec3[] ppt )
     {
+        pnts.setData( [from] ~ ppt );
+
+        auto transform = mapmtr.inv * matrix.inv;
+
         glFlush();
-        clmem.acquireFromGL( cmdqueue );
+        glFinish();
 
-        //update.setArgs();
-        //update.exec( cmdqueue, 1, [0], [1024 * 2], [32] );
+        data.acquireFromGL();
+        pnts.acquireFromGL();
 
-        clmem.releaseToGL( cmdqueue );
+        update.setArgs( data.clmem, to!(uint[4])( mres.data ~ 0 ),
+                        pnts.clmem, cast(uint)pnts.elementCount,
+                        cast(float[16])transform.asArray[0..16]
+                       );
+        update.exec( cmdqueue, 1, [0], [32], [16] );
+
+        pnts.releaseToGL();
+        data.releaseToGL();
+
         cmdqueue.flush();
     }
 
@@ -114,27 +152,24 @@ protected:
 
     override void prepareBuffers()
     {
-        data = registerChildEMM( new GLBuffer( GLBuffer.Target.ARRAY_BUFFER ) );
+        auto cnt = mres.w * mres.h * mres.d;
+
+        data = registerChildEMM( new GLCLBuffer( new int[](cnt) ) );
         auto loc = shader.getAttribLocation( "data" );
         setAttribPointer( data, loc, 1, GLType.INT );
-        auto cnt = mres.w * mres.h * mres.d;
-        data.setData( new int[](cnt), GLBuffer.Usage.DYNAMIC_DRAW );
 
-        pnts = registerChildEMM( new GLBuffer( GLBuffer.Target.ARRAY_BUFFER ) );
-
-        clmem = CLGLMemory.createFromGLBuffer( ctx, CLMemory.Flags.READ_WRITE, data );
+        pnts = registerChildEMM( new GLCLBuffer( new vec3[](1) ) );
     }
 }
 
 enum CLSource = 
 `
-inline float3 mlt( global float4* m, float3 v )
+inline float3 mlt( const float16 m, float3 v )
 {
-    float4 p = (float4)(v,1);
-    return (float3)( dot( m[0], p ),
-                     dot( m[1], p ),
-                     dot( m[2], p ) );
-
+    return (float3)( 
+    m.s0 * v.x + m.s1 * v.y + m.s2 * v.z + m.s3,
+    m.s4 * v.x + m.s5 * v.y + m.s6 * v.z + m.s7,
+    m.s8 * v.x + m.s9 * v.y + m.sa * v.z + m.sA );
 }
 
 inline bool inRegionI( const uint3 size, const uint3 pnt )
@@ -154,10 +189,14 @@ inline bool inRegionF( const uint3 size, const float3 pnt )
 inline size_t index( const uint3 size, const uint3 pnt )
 { return pnt.x + pnt.y * size.x + pnt.z * size.x * size.y; }
 
-kernel void update( global int* data, const uint3 size, global float3* pnts, const uint count, global float4* mtr )
+kernel void update( global uint* map, const uint4 esize,
+                    global float3* pnts, const uint count,
+                    const float16 mtr )
 {
     int i = get_global_id(0);
     int sz = get_global_size(0);
+
+    uint3 size = (uint3)(esize.xyz);
 
     float3 mfrom = mlt( mtr, pnts[0] );
     for( ; i < count+1; i+=sz )
@@ -174,9 +213,9 @@ kernel void update( global int* data, const uint3 size, global float3* pnts, con
                 float3 v = mfrom + nvv * i;
                 uint3 vcrd = (uint3)( v.x, v.y, v.z );
                 if( inRegionI( size, vcrd ) )
-                    data[index(size,vcrd)] = 1;
+                    map[index(size,vcrd)] = 1;
             }
-            data[index(size,crd)] = 2;
+            map[index(size,crd)] = 2;
         }
     }
 }
